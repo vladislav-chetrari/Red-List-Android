a4188af0f60897b722bc652eeb987f86489c342c
Chetrari Vlad <i79182602@gmail.com>
2020-05-31 17:14:14 +0300

    converter moved to separate package

--- app/src/main/java/chetrari/vlad/redlist/data/persistence/model/Species.kt
+++ app/src/main/java/chetrari/vlad/redlist/data/persistence/model/Species.kt
@@ -1,12 +1,12 @@
 package chetrari.vlad.redlist.data.persistence.model
 
+import chetrari.vlad.redlist.data.persistence.model.converter.VulnerabilityConverter
 import chetrari.vlad.redlist.data.persistence.type.Vulnerability
 import chetrari.vlad.redlist.data.persistence.type.Vulnerability.NE
 import io.objectbox.annotation.Convert
 import io.objectbox.annotation.Entity
 import io.objectbox.annotation.Id
 import io.objectbox.annotation.Unique
-import io.objectbox.converter.PropertyConverter
 import io.objectbox.relation.ToMany
 import io.objectbox.relation.ToOne
 
@@ -32,9 +32,4 @@ data class Species(
     lateinit var images: ToMany<SpeciesImage>
     lateinit var countries: ToMany<Country>
     lateinit var narrative: ToOne<Narrative>
-}
-
-class VulnerabilityConverter : PropertyConverter<Vulnerability, String> {
-    override fun convertToEntityProperty(databaseValue: String?) = databaseValue?.let(Vulnerability::valueOf) ?: NE
-    override fun convertToDatabaseValue(entityProperty: Vulnerability?) = (entityProperty ?: NE).toString()
 }
